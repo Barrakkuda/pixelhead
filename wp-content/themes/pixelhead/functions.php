ad0f335af27d3d69ba11ed96371d44ffd3dfe47d
Luis F. Salazar <32740115+luisfsalazar@users.noreply.github.com>
2018-03-14 14:50:50 -0400

    Create the child theme

---
+++ wp-content/themes/pixelhead/functions.php
@@ -0,0 +1,14 @@
+<?php
+// Add your custom functions here
+
+// Queue parent style followed by child/customized style
+add_action( 'wp_enqueue_scripts', 'pixelhead_enqueue_child_styles', 99);
+
+function pixelhead_enqueue_child_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_dequeue_style('wp-bootstrap-starter-style');
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
